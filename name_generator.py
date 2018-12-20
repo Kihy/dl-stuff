17c5f425a5db26ce7bf167aa57b6c9f0dbfa46e8
kihy <kh695640204@gmail.com>
2018-12-20 10:18:27 +0800

    fixed bug with name_generator

--- name_generator.py
+++ name_generator.py
@@ -4,7 +4,7 @@ out_dir="dataset/testFire/ImageSet"
 
 out_file=open(os.path.join(out_dir,"names.txt"),"w")
 for i in os.listdir(image_dir):
-    if os.path.isfile(i):
+    if os.path.isfile(os.path.join(image_dir,i)):
         out_file.write(i+"\n")
 
 out_file.close()
