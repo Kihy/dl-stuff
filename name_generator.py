d8ef38f83f8dee65d863bacdd21aeb53ca882c03
kihy <kh695640204@gmail.com>
2018-12-20 10:13:41 +0800

    added helper script for filename generation

---
+++ name_generator.py
@@ -0,0 +1,10 @@
+import os
+image_dir="dataset/testFire"
+out_dir="dataset/testFire/ImageSet"
+
+out_file=open(os.path.join(out_dir,"names.txt"),"w")
+for i in os.listdir(image_dir):
+    if os.path.isfile(i):
+        out_file.write(i+"\n")
+
+out_file.close()
